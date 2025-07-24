f19070c94851dd10f16bbd78f02860ea3fca83b1
mouad <mouad464@gmail.com>
2025-07-24 17:07:26 +0900

    Add more animations for NBV algorithm

--- view_frustum.py
+++ view_frustum.py
@@ -2,6 +2,78 @@ from manim import *
 import numpy as np
 
 class NBVVisualization(Scene):
+    def create_entropy_animation(scene):
+        # LaTeX formula
+        formula = MathTex(
+            r"I_v(x) = -p(x)\log_2(p(x)) - (1-p(x))\log_2(1-p(x))",
+            font_size=36
+        ).to_edge(UP)
+
+        # Create axes
+        axes = Axes(
+            x_range=[0, 1, 0.1],
+            y_range=[0, 1.2, 0.2],
+            x_length=8,
+            y_length=4,
+            axis_config={"color": BLUE},
+            x_axis_config={"numbers_to_include": np.arange(0, 1.1, 0.2)},
+            y_axis_config={"numbers_to_include": np.arange(0, 1.2, 0.2)}
+        ).shift(DOWN * 0.5)
+
+        # Axis labels
+        x_label = axes.get_x_axis_label("p(x)")
+        y_label = axes.get_y_axis_label("I_v(x)")
+
+        # Entropy function
+        def entropy(p):
+            if p <= 0 or p >= 1:
+                return 0
+            return -p * np.log2(p) - (1-p) * np.log2(1-p)
+
+        # Plot the function
+        graph = axes.plot(
+            lambda x: entropy(x) if 0 < x < 1 else 0,
+            x_range=[0.001, 0.999],
+            color=YELLOW
+        )
+
+        # Sliding point
+        t_tracker = ValueTracker(0.1)
+        point = always_redraw(lambda: Dot(
+                                  axes.c2p(t_tracker.get_value(), entropy(t_tracker.get_value())),
+                                  color=RED,
+                                  radius=0.08
+                              ))
+
+        # Point coordinates text
+        coords = always_redraw(lambda: MathTex(
+                                   f"({t_tracker.get_value():.2f}, {entropy(t_tracker.get_value()):.2f})",
+                                   font_size=24
+                               ).next_to(point, UR, buff=0.1))
+
+        # Animations
+        scene.play(Write(formula))
+        scene.play(Create(axes), Write(x_label), Write(y_label))
+        scene.play(Create(graph))
+        scene.play(Create(point), Write(coords))
+
+        # Animate sliding point
+        scene.play(t_tracker.animate.set_value(0.9), run_time=4, rate_func=linear)
+        scene.play(t_tracker.animate.set_value(0.1), run_time=4, rate_func=linear)
+        scene.play(t_tracker.animate.set_value(0.5), run_time=2)
+
+        scene.wait(2)
+
+        scene.play(
+            Uncreate(point),
+            Unwrite(coords),
+            Uncreate(graph),
+            Uncreate(axes),
+            Unwrite(x_label),
+            Unwrite(y_label),
+            Unwrite(formula)
+        )
+
     def construct(self):
         # Camera position and parameters
         camera_pos = np.array([-4, 0, 0])
@@ -56,6 +128,10 @@ class NBVVisualization(Scene):
         voxel_positions = [
             np.array([0, 0, 0]),
             np.array([0, 0.5, 0]),
+            np.array([0.5, 0.5, 0]),
+            np.array([0.5, 0, 0]),
+            np.array([0.5, -0.5, 0]),
+            np.array([0.5, -1, 0]),
             np.array([0.5, 1, 0]),
             np.array([1, 1, 0]),
             np.array([0, 0.5, 0]),
@@ -65,6 +141,9 @@ class NBVVisualization(Scene):
             np.array([0, -0.5, 0]),
             np.array([0, -1, 0]),
             np.array([-0.5, -1.5, 0]),
+            np.array([0,  -1.5, 0]),
+            np.array([0.5,  -1.5, 0]),
+            np.array([1,  -1.5, 0]),
         ]
 
         # Check which voxels are hit by rays
@@ -90,7 +169,7 @@ class NBVVisualization(Scene):
             voxel.move_to(pos)
 
             if pos.tolist() in [p.tolist() for p in hit_positions]:
-                voxel.set_color(RED)
+                # voxel.set_color(RED)
                 hit_voxels.add(voxel)
             else:
                 visible_voxels.add(voxel)
@@ -118,7 +197,7 @@ class NBVVisualization(Scene):
             occluded_voxels.add(dashed_voxel)
 
         # Add title
-        title = Text("NBV Algorithm: View Cone with Uniform Rays", font_size=28, color=WHITE)
+        title = Text("NBV Algorithm", font_size=28, color=WHITE)
         title.to_edge(UP)
 
         # Add legend
@@ -143,28 +222,105 @@ class NBVVisualization(Scene):
             legend.add(item)
 
         # Animation sequence
-        self.play(Write(title))
+        # self.play(Write(title))
+        # self.wait(0.5)
+
+        titlePart = Text("Plant (object) is represented as voxels", font_size=28, color=WHITE)
+        titlePart.move_to(UP * 2.5)
+        self.play(Write(titlePart))
+        # Add voxels
+        self.play(Create(visible_voxels), Create(hit_voxels), Create(occluded_voxels), run_time=2)
         self.wait(0.5)
 
+
+        # What is a voxel part
+        self.play(Unwrite(titlePart))
+        titlePart = Tex(
+            r"Each voxel $x$ holds an occupancy probability $p(x)$",
+            font_size=28,
+            color=WHITE
+        )
+        titlePart.move_to(UP * 2.5)
+        self.play(Write(titlePart))
+        smallX = Tex(
+            r"$x$",
+            font_size=32
+        )
+        smallX.move_to(RIGHT*3 + DOWN)
+
+        arrow = Arrow(
+            start=smallX.get_center(),
+            end=hit_voxels[5].get_center(),
+            color=WHITE
+        )
+        self.play(Write(smallX), Create(arrow))
+        self.wait(0.5)
+        text_block = MathTex(
+            r"p(x) = 0 \rightarrow \text{ certainly empty} \\",
+            r"p(x) = 0.5 \rightarrow \text{ uncertain} \\",
+            r"p(x) = 1 \rightarrow \text{ certainly occupied}",
+            font_size=32,
+            color=WHITE
+        ).to_edge(LEFT)
+        self.play(Write(text_block))
+        self.wait(1)
+        self.play(Unwrite(text_block), Uncreate(arrow), Uncreate(smallX))
+        self.play(Uncreate(visible_voxels), Uncreate(hit_voxels), Uncreate(occluded_voxels), run_time=2)
+
+        # What is information gain
+        self.play(Unwrite(titlePart))
+        titlePart = Tex(
+            r"Each voxel $x$ has an information function $I_v$",
+            font_size=28,
+            color=WHITE
+        )
+        titlePart.to_edge(UP)
+        self.play(Write(titlePart))
+        self.create_entropy_animation()
+        self.wait(1)
+        text_block = MathTex(
+            r"p(x) = 0 \rightarrow \text{ certainly empty} \rightarrow I_v(x) \text{ will be lowest} \\",
+            r"p(x) = 0.5 \rightarrow \text{ uncertain} \rightarrow I_v(x) \text{ will be highest} \\",
+            r"p(x) = 1 \rightarrow \text{ certainly occupied} \rightarrow I_v(x) \text{ will be lowest}",
+            font_size=32,
+            color=WHITE
+        ).to_edge(LEFT)
+        self.play(Write(text_block))
+        self.wait(1)
+        self.play(Unwrite(text_block))
+
+        # First Part
+        self.play(Unwrite(titlePart))
+        titlePart = Text("Camera is represented as a view cone", font_size=28, color=WHITE)
+        titlePart.move_to(UP * 2.5)
         # Add camera
+        self.play(Write(titlePart))
         self.play(Create(camera_point), Write(camera_label))
         self.wait(0.5)
 
         # Add view cone
         self.play(Create(view_cone), Create(upper_boundary), Create(lower_boundary))
         self.wait(0.5)
+        self.wait(1)
 
+        #Second part
+        self.play(Unwrite(titlePart))
+        titlePart = Text("Rays are casted from the camera origin", font_size=28, color=WHITE)
+        titlePart.move_to(UP * 2)
+        self.play(Write(titlePart))
         # Add rays (uniform, representing pixel resolution)
         self.play(Create(rays), run_time=2)
         self.wait(0.5)
 
+        self.play(Unwrite(titlePart))
+        titlePart = Text("Rays intersect with voxels", font_size=28, color=WHITE)
+        titlePart.move_to(UP * 2)
+        self.play(Write(titlePart))
         # Add voxels
-        self.play(Create(visible_voxels), Create(occluded_voxels), run_time=2)
+        self.play(Create(visible_voxels), Create(hit_voxels), Create(occluded_voxels), run_time=2)
         self.wait(0.5)
 
         # Highlight ray-hit voxels
-        self.play(Create(hit_voxels), run_time=1.5)
-        self.wait(0.5)
 
         # Add legend
         self.play(Create(legend), run_time=2)
